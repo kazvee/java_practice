33f5efab8cf6af5da07bfa5ae154e66ff6f5d7fe
kazvee <kazveedev@gmail.com>
2024-10-09 12:38:13 -0600

    Add Introduction

---
+++ Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args) {
+
+        String name = "Gudetama";
+        String hobbies = "napping, relaxing, and thinking about napping";
+        int age = 11;
+
+        System.out.println("My name is " + name);
+        System.out.printf("I am %d years old\n", age);
+        System.out.print("My hobbies are " + hobbies);
+
+    }
+}
