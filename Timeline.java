4391af2dde69f962fd2ebc8defb4207f0b92d8aa
kazvee <kazveedev@gmail.com>
2024-10-09 13:23:26 -0600

    Add Timeline

---
+++ Timeline.java
@@ -0,0 +1,20 @@
+public class Timeline {
+    public static void main(String[] args) {
+        System.out.println("Hello Gudetama!");
+
+        System.out.println("You were born in 2013.");
+
+        /*
+         * Gudetama is a fictional character created in 2013
+         * by the Japanese company Sanrio
+         * and is a perpetually tired, apathetic anthropomorphic egg yolk.
+         */
+
+        System.out.println("You are a perpetually tired, apathetic anthropomorphic egg yolk.");
+
+        // Gudetama is a perpetually tired, apathetic anthropomorphic egg yolk.
+
+        System.out.println(
+                "The name \"Gudetama\" is a portmanteau or blend word of the Japanese words for lazy and egg.");
+    }
+}
